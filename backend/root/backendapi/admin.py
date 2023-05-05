554eb4eedb4b1bd48bb3aa5eb7e3aae6fc12b76f
Olazs <warplanes96@hotmail.com>
2023-05-05 18:08:18 +0200

    SQLite3 database migrations+admin panel fill

--- backend/root/backendapi/admin.py
+++ backend/root/backendapi/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Event, Refuel
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Refuel)
--- backend/root/backendapi/models.py
+++ backend/root/backendapi/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    mileage = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    price = models.IntegerField(default=0)
+    date=models.DateField(auto_now=True)
+
+class Refuel(Event):
+    quantity=models.IntegerField()
+    gas_station=models.CharField(max_length=50, null=True, blank=True)
+
+
+
