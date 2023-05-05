95f00c59e8f97be84969874003ab5f69effbce9d
Olazs <warplanes96@hotmail.com>
2023-05-05 18:17:09 +0200

    admin panel: Gas stations have been added

--- backend/root/backendapi/admin.py
+++ backend/root/backendapi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Event, Refuel
+from .models import Event, Refuel, GasStation
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Refuel)
+admin.site.register(GasStation)
