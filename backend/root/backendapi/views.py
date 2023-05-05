6f9547694e9783de8f496dee7031e10fc33cf646
Olazs <warplanes96@hotmail.com>
2023-05-05 18:37:50 +0200

    Base API for getting all the refuels from the database.

--- backend/root/backendapi/views.py
+++ backend/root/backendapi/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Refuel
+from .serializers import RefuelSerializer
 # Create your views here.
 
+
+def GetAllRefuels(request): #all refuel listed out.
+    elements = Refuel.objects.all()
+    ser = RefuelSerializer(elements, many=True)
+    return JsonResponse(ser.data, safe=False)
