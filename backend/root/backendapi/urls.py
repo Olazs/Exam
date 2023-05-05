0e0b8860b80df346965a59a90b8822155c70f3dd
Olazs <warplanes96@hotmail.com>
2023-05-05 18:51:58 +0200

    Single Refuel query added at /Refuel api

--- backend/root/backendapi/urls.py
+++ backend/root/backendapi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('refuels/', views.GetAllRefuels)
+    path('refuels/', views.GetAllRefuels),
+    path('refuel/<int:pk>', views.GetTheRefuelById)
 ]
--- backend/root/backendapi/views.py
+++ backend/root/backendapi/views.py
@@ -1,10 +1,21 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Refuel
 from .serializers import RefuelSerializer
 # Create your views here.
 
 
+@api_view(['GET'])
 def GetAllRefuels(request): #all refuel listed out.
     elements = Refuel.objects.all()
     ser = RefuelSerializer(elements, many=True)
-    return JsonResponse(ser.data, safe=False)
+    return Response(ser.data)
+
+@api_view(['GET'])
+def GetTheRefuelById(request, pk): #single refuel 
+    try:
+        element =Refuel.objects.get(id=pk)
+        ser =RefuelSerializer(element, many=False)
+        return Response(ser.data)
+    except Exception as e:
+        return Response({'message':str(e)})
