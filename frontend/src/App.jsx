e8189743e1f3a46ef56d2aa0d649465bf1d84b2d
Olazs <warplanes96@hotmail.com>
2023-05-06 18:14:17 +0200

    Frontend begins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+//import "./styles.css"
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      <h1>Car Costs Log Webapp</h1>
+    <div className='App'>
+     <form className="new-item-form">
+      <h1 htmlFor="">New Log</h1>
+      <div className="form-row">
+        
+        <input type="number" name='Mileage' id='Mileage' placeholder='Mileage' /> 
+        <input type="text" name='Description' id="Description" placeholder='Description'/>
+        <input type="number" name="Price" id="Price" placeholder='Price' />
+        <input type="text" name='Date' id="Date" placeholder='Date' />
+        <input type="number" name="Quantity" id="Quantity" placeholder='Quantity'/>
+        <input type="text" name="GasStationType" id='GasStationType' placeholder='GasStationType'/>
+        <br />
+        <input type="button" value="Add" />
+      </div>
+      </form>
     </div>
   )
 }
